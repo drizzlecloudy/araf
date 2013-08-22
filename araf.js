f5fc6181532ffe43b84d406dbed0bedfe8c2a545
drizzlecloudy <drizzlecloudy@gmail.com>
2013-08-23 05:55:05 +0700

    Update araf.js

--- araf.js
+++ araf.js
@@ -1,4 +1,6 @@
-/*! Heavily inspired by:
+/*! Originally by Erik Möller http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+ *  and my little project was heavily inspired by:
+ *
  *  Paul Irish writing on https://gist.github.com/paulirish/1579671
  *  Nicolas Gryman project on https://github.com/ngryman/raf.js
  *  Klemen Slavič 'fixing' on https://gist.github.com/KrofDrakula/5318048
